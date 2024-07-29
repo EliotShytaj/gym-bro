2fc709c593f6ac2f3dc7007884636fddeea2e02b
Eliot Shytaj <eliotshytaj05@gmail.com>
2024-07-29 16:27:27 -0400

    Creating the workout schema

--- server/models/Workout.js
+++ server/models/Workout.js
@@ -1,2 +1,18 @@
 const mongoose = require("mongoose")
 
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    weight: { 
+        type: Number,
+        required: true
+    }
+}, { timestamps: true })
