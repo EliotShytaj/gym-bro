7e56cc2de2fd7657a110f63233976cc08166f9d3
Eliot Shytaj <eliotshytaj05@gmail.com>
2024-07-29 15:14:44 -0400

    added API endpoints

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
 require("dotenv").config()
 
 const express = require("express")
+const workoutRoutes = require("./routes/workouts")
 
 //this creates an express app 
 const app = express()
 
 //middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
 //routes
-app.get("/", (req,res) => {
-    res.json({mssg: "Welcome to the app"})
-})
+app.use("/api/workouts", workoutRoutes)
 
 
 // listen for requests 
 app.listen(process.env.PORT, () =>  {
-    console.log("Listening on port 4001")
+    console.log("Listening on port", process.env.PORT)
 })
